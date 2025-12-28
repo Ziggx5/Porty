ef54b3282461601ffddce4da67e64af1d3dcf5a5
Maj Sedonja <maj.sedonja5@gmail.com>
2025-12-29 00:09:49 +0100

    More updates on UI

--- modules/ui.py
+++ modules/ui.py
@@ -3,7 +3,7 @@ import socket
 
 def start_ui():
     app = CTk()
-    app.geometry("600x500")
+    app.geometry("895x700")
     app.title("Porty")
     app.resizable(False, False)
 
@@ -13,16 +13,16 @@ def start_ui():
     scan_button = CTkButton(app, text = "Scan", fg_color = "#6398ba", width = 100)
     scan_button.place(x = 220, y = 10)
 
-    closed_label = CTkLabel(app, text = "Closed ports", font = ("TkTextFont", 20), text_color = "white")
-    closed_label.place(x = 10, y = 60)
-
     closed_textbox = CTkTextbox(app, font = ("TkTextFont", 15), text_color = "white", width = 285, height = 250)
     closed_textbox.place(x = 10, y = 100)
-
-    open_label = CTkLabel(app, text = "Open ports", font = ("TkTextFont", 20), text_color = "white")
-    open_label.place(x = 300, y = 60)
 
     open_textbox = CTkTextbox(app, font = ("TkTextFont", 15), text_color = "white", width = 285, height = 250)
     open_textbox.place(x = 305, y = 100)
 
+    misc_textbox = CTkTextbox(app, font = ("TkTextFont", 15), text_color = "white", width = 285, height = 250)
+    misc_textbox.place(x = 600, y = 100)
+
+    logs_textbox = CTkTextbox(app, font = ("TkTextFont", 15), text_color = "white", width = 875, height = 250)
+    logs_textbox.place(x = 10, y = 380)
+    
     app.mainloop()
